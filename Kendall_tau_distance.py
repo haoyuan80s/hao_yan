c4d7a51159425a19dfa263a565b99774faa24d53
Hao Yuan <haoyuan80s@gmail.com>
2017-04-14 21:17:11 -0400

    add comments

--- Kendall_tau_distance.py
+++ Kendall_tau_distance.py
@@ -3,6 +3,7 @@ a = [1,2,3,4,5,6]
 b = a[:]; random.shuffle(b)
 
 def K(a,b):
+    """measure ranking distance to lists a,b"""
     n = len(a)
     return sum( [  (a[i] < a[j] and b[i] > b[j])
                    or (a[i] > a[j] and b[i] < b[j])
@@ -10,6 +11,11 @@ def K(a,b):
                    for j in range(i+1, n)])
 
 def Kpp(a,b):
+    """
+    measure ranking distance to lists a,b,
+    with panlize more low ranking mismatch 
+    and high dist mismatch
+    """
     n = len(a)
     return sum([(j-i)/(j+i)*((a[i] < a[j] and b[i] > b[j])
                    or (a[i] > a[j] and b[i] < b[j]))
